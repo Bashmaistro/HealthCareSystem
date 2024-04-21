0ebdc41aab9790dcf87b82ac2653c04bbdb9fe48
solauphoenix <96625984+solauphoenix@users.noreply.github.com>
2024-04-21 22:17:57 +0300

    template adjusted partially
    getting the idea of thymyleaf

--- src/main/java/com/healthcaresystem/TestController.java
+++ src/main/java/com/healthcaresystem/TestController.java
@@ -7,7 +7,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class TestController {
     @GetMapping
-    String getPeople(Model model){
-        return "people";
+    String getMedify(Model model){
+        model.addAttribute("title","Medify");
+        return "index";
     }
 }
